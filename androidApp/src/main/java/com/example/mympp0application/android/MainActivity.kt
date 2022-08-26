87d278fffb6c79d0fe9116970306b2b4ba137ae5
moocstudent <deadzq@qq.com>
2022-08-26 20:01:32 +0800

    add README.MD add API response of SpaceX

--- androidApp/src/main/java/com/example/mympp0application/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/mympp0application/android/MainActivity.kt
@@ -4,17 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mympp0application.Greeting
 import android.widget.TextView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-fun greet(): String {
-    return Greeting().greeting()
-}
+//
+//fun greet(): String {
+//    return Greeting().greeting()
+//}
 
 class MainActivity : AppCompatActivity() {
+    private val scope = MainScope()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()+"version1.0"
+        tv.text = "Loading..."
+
+        scope.launch {
+            kotlin.runCatching{
+                /**
+                 * The greeting() function is now called
+                 * inside the coroutine launched in the main
+                 * CoroutineScope
+                 */
+                Greeting().greeting()
+            }.onSuccess {
+                tv.text=it
+            }.onFailure {
+                tv.text=it.localizedMessage
+            }
+        }
     }
 }
--- shared/src/commonMain/kotlin/com/example/mympp0application/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/mympp0application/Greeting.kt
@@ -1,8 +1,32 @@
 package com.example.mympp0application
 
+import com.example.mympp0application.entity.RocketLaunch
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
 class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform.reversed()}!"+
-                "\nThere are only ${daysUntilNewYear()} left until New Year!"
+
+    private val httpClient= HttpClient{
+        install(ContentNegotiation){
+            json(Json{
+                prettyPrint=true
+                isLenient=true
+                ignoreUnknownKeys=true
+            })
+        }
     }
+
+    @Throws(Exception::class)
+    suspend fun greeting(): String {
+        val rockets:List<RocketLaunch> = httpClient.get("https://api.spacexdata.com/v4/launches").body()
+        val lastSuccessLaunch = rockets.last {it.launchSuccess==true}
+        return "Hello, ${Platform().platform.reversed()}!"+
+                "\nThere are only ${daysUntilNewYear()} left until New Year!"+
+                "\nThe last successful launch was ${lastSuccessLaunch.launchDateUTC} ð"
+    }
+
 }
